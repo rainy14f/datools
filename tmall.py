45060484583b8f0d4064cd3663c97f91e5cc9080
rainy14f <fshao@outlook.com>
2016-04-28 08:36:25 +0800

    Updated

--- tmall.py
+++ tmall.py
@@ -5,6 +5,8 @@ import csv, sqlite3
 import logging
 
 
+DB_NAME = 'tmall_skuid.db'
+
 def _get_col_datatypes(fin):
     dr = csv.DictReader(fin) # comma is default delimiter
     fieldTypes = {}
@@ -74,21 +76,22 @@ def csv_to_db(csvFile, outputToFile = False):
             cols.append("%s %s" % (f, dt[f]))
 
         # insert IDs
-        cols.insert(23, 'SkuID')
-        cols.insert(24, 'ID')
-        cols.insert(25, 'RPC')
+        cols.insert(23, 'skuid')
+        cols.insert(24, 'id')
+        cols.insert(25, 'rpc')
 
         table_name = 'ads7'
 
         # Generate create table statement:
-        stmt = "CREATE TABLE %s (%s)" % (table_name, ",".join(cols))
-
-        con = sqlite3.connect("f:/dev/csv.db")
+        con = sqlite3.connect(DB_NAME)
         con.text_factory = lambda x: unicode(x, "utf-8", "ignore")
         cur = con.cursor()
+
+        cur.execute('DROP TABLE IF EXISTS %s' % (table_name))
+        stmt = "CREATE TABLE %s (%s)" % (table_name, ",".join(cols))
         cur.execute(stmt)
 
-        fin.next()
+        #fin.next()
 
         reader = csv.reader(fin)
 
@@ -100,7 +103,8 @@ def csv_to_db(csvFile, outputToFile = False):
 
 
 def analyse(csv_file_path):
-    con = sqlite3.connect("f:/dev/csv.db")
+    #con = sqlite3.connect("f:/dev/csv.db")
+    con = sqlite3.connect(DB_NAME)
     cur = con.cursor()
 
     output = []
@@ -113,18 +117,33 @@ def analyse(csv_file_path):
         header.insert(26, 'id')
         header.insert(27, 'skuId')
         header.insert(28, 'RPC')
+        header.insert(29, 'Changed')
         output.append(header)
 
         for line in reader:
             skuid, id, rpc = parse_url(line[3])
-            cur.execute('select skuid, id, rpc from ads7 where id=:id', {'id':id})
-            prev_skuid, prev_id, prev_rpc = cur.fetchone()
+            if skuid:
+                cur.execute('select skuid, id, rpc from ads7 where skuid=:skuid', {'skuid': skuid})
+                res = cur.fetchone()
+                if res:
+                    prev_skuid, prev_id, prev_rpc = res[0], res[1], res[2]
+            else:
+                cur.execute('select skuid, id, rpc from ads7 where id=:id', {'id':id})
+                print cur.rowcount
+                res = cur.fetchone()
+                if res:
+                    prev_skuid, prev_id, prev_rpc = res[0], res[1], res[2]
+                else:
+                    print '(skuid: %s, id: %s) is a new SKU' % (skuid, id)
+                    prev_skuid, prev_id, prev_rpc = ('', '', '')
+
             line.insert(23, prev_id)
             line.insert(24, prev_skuid)
             line.insert(25, prev_rpc)
             line.insert(26, id)
             line.insert(27, skuid)
             line.insert(28, rpc)
+            line.insert(29, 'x' if rpc!=prev_rpc else '')
             output.append(line)
 
     con.close()
@@ -146,7 +165,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-#csv_to_db('f:\\Tmall_Skii_by_brand.csv')
-analyse('f:\\Tmall_Skii_by_brand.csv')
+csv_to_db('f:\\Tmall_Dove_byCategory-2016-03.csv')
+analyse('f:\\Tmall_Dove_byCategory-2016-04.csv')
 
 
